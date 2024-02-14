a0e62edbca1efc75eec3a46257d7120e772d1515
Karmel Idarraga <kidarraga@euro-help.es>
2024-02-14 10:58:30 +0100

    Cambios para adaptar a las nuevas instrucciones de nombre de la BBDD y el usuario

--- idarraga_arino_karmel_dwes04 .sql
+++ idarraga_arino_karmel_dwes04 .sql
@@ -1,5 +1,5 @@
-CREATE DATABASE  IF NOT EXISTS `dwesbd04` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci */;
-USE `dwesbd04`;
+CREATE DATABASE  IF NOT EXISTS `idarraga_arino_karmel_dwes04` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci */;
+USE `idarraga_arino_karmel_dwes04`;
 -- MySQL dump 10.13  Distrib 8.0.34, for Win64 (x86_64)
 --
 -- Host: 127.0.0.1    Database: dwesbd04
